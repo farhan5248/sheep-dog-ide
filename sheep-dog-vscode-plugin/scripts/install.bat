cef723c7057e8623f50f85069f50d7047fc71052
Farhan Sheikh <farhan.sheikh.5248@gmail.com>
2026-03-26 21:35:12 -0400

    Create Standalone Asciidoc Grammar

--- sheep-dog-vscode-plugin/scripts/install.bat
+++ sheep-dog-vscode-plugin/scripts/install.bat
@@ -1,5 +1,6 @@
 cd ..
 call npm install
+if not exist build mkdir build
 call npx vsce package --out build/sheep-dog-vscode-plugin.vsix
 call code --install-extension build/sheep-dog-vscode-plugin.vsix
 cd scripts
